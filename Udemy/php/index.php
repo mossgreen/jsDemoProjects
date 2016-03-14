18c7be8b422f92b48c9bf411f45cdd7cea316115
Moss Green <gufeifei@outlook.com>
2016-03-14 22:07:44 +1300

    for loop in php

--- Udemy/php/index.php
+++ Udemy/php/index.php
@@ -1,9 +1,92 @@
 <?php
-	echo "welcome to moss.";
 
-	$test = "I'm a variable!"
+echo" --how to define arrays in php--";
 
-	echo $test;
+echo"<br /><br />";
+
+
+
+$myArray = array("pizza", "chocolate", "coffee");
+
+echo $myArray;
+echo"<br /><br />";
+
+print_r($myArray);
+echo"<br /><br />";
+
+
+echo $myArray[2];
+
+echo"<br /><br />";
+
+$anotherArray[0] = "pizza";
+$anotherArray[2] = "coffee"; //test
+
+print_r($anotherArray);
+
+echo"<br /><br />";
+
+$thirdArray = array(
+	"France" => "French",
+	"USA" => "English",
+	"Germany" => "German"
+);
+
+print_r($thirdArray);
+echo"<br /><br />";
+
+
+$anotherArray[] = "salad";
+print_r($anotherArray);
+echo"<br /><br />";
+
+
+unset($thirdArray["Germany"]);
+print_r($thirdArray);
+echo"<br /><br />";
+
+
+$name="Rob";
+unset($name);
+echo $name;
+
+echo"<br /><br />";
+
+echo "-- if statement--";
+echo"<br /><br />";
+
+
+$thisNumber = 1;
+$thatNumber = 2;
+$thirdNumber = 2;
+if ($number != $thatNumber AND $thatNumber == $thirdNumber){
+	echo "true";
+}else{
+	echo "false";
+}
+
+//will print "true"
+echo"<br /><br />";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
