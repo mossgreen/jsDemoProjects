0204f3eb5c3d73a754ac1f43af0539a82ebcee27
Moss Green <gufeifei@outlook.com>
2016-03-15 12:10:28 +1300

    server side form validation in php
    what i learnt in this project:
    1. server side validation, php has its own email validation:
    	filter_var($_POST['email'], FILTER_VALIDATE_EMAIL
    
    2. send a email on a page:
    
    	$sent = mail("unfeifei@gmail.com", "Comment from website!", "Name:
    ".$_POST["name"]
    			."Email: ".$_POST["email"]."
    			Comment: ".$_POST["comment"]
    		);
    3. store value, or say data, in the page after submiting to server is
    to add a value to the input
    	and add a slice of php code like:
    
    	<input type="text" name="email" class="from-control" placeholder="your
    name" value="<?php echo $_POST['email']; ?>" />

--- Udemy/php/email.php
+++ Udemy/php/email.php
@@ -1,3 +1,77 @@
+<?php
+
+/*
+what i learnt in this project:
+1. serverside validation, php has its own email validation:
+	filter_var($_POST['email'], FILTER_VALIDATE_EMAIL
+
+2. send a email on a page:
+	
+	$sent = mail("unfeifei@gmail.com", "Comment from website!", "Name: ".$_POST["name"]
+			."Email: ".$_POST["email"]."
+			Comment: ".$_POST["comment"]
+		);
+3. store value, or say data, in the page after submiting to server is to add a value to the input
+	and add a slice of php code like:
+	
+	<input type="text" name="email" class="from-control" placeholder="your name" value="<?php echo $_POST['email']; ?>" />
+	
+		
+
+
+*/
+
+	if($_POST["submit"]){
+		//$result="Form submitted";
+		$result = '<div class="alert alert-success">Form submitted</div>';
+	}
+	
+	if(!$_POST["name"]){
+	
+		$error.= "<br />name";
+	}
+
+	//if(!$_POST["email"]){
+	
+	//	$error.= "<br />email";
+	//}
+
+	if(!$_POST["comment"]){
+	
+		$error.= "<br />comment";
+	}
+	
+	//validate the email address: if it's entered AND validated
+	if($_POST['email'] == "" OR !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+		$error.="<br />please enter a valid email address.";
+	}
+	
+	$sent = mail("unfeifei@gmail.com", "Comment from website!", "Name: ".$_POST["name"]
+			."Email: ".$_POST["email"]."
+			Comment: ".$_POST["comment"]
+		);
+	
+	
+	if($error){
+
+		$result = '<div class="alert alert-danger"><strong>Error, please check: '.$error.'</strong></div>';
+	}else{
+	
+		if($sent){ 
+			$result = '<div class="alert alert-success"><strong>Thanks</strong></div>';
+		}else{
+			$result =  '<div class="alert alert-success"><strong>Sorry, try again later.</strong></div>';
+		}
+
+	}
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,14 +82,58 @@
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<style>
+	.emailForm{
+		border:1px solid grey;
+		border-radius: 10px;
+		margin-top:20px;
+	}
+	
+	textarea{
+		height:120px;
+	}
+	
+	form{
+		padding-bottom: 20px;
+	}
+
+</style>
+
 </head>
 <body>
 
-<h1>this is moss's first page</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 emailForm">
+				<h1>My eMail Form</h1>
+				
+				<?php echo $result ?>				
+				<p class="lead">Please get in touch- i'll get back to you as soon as possible."</p>
+				
+				<form method="post">
+					<div class="form-group">
+						<label for="name">Your Name:</label>
+						<input type="text" name="name" class="from-control" placeholder="your name" value="<?php echo $_POST['name']; ?>" />
+					</div>
+				
+					<div class="form-group">
+						<label for="name">Your email:</label>
+						<input type="text" name="email" class="from-control" placeholder="your name" value="<?php echo $_POST['email']; ?>" />
+					</div>
+					
+					<div class="form-group">
+						<label for="name">Your Comment:</label>
+						<textarea class="form-control" name="comment" value="<?php echo $_POST['comment']; ?>"></textarea>
+					</div>		
+					
+					<input type="submit" name="submit" class="btn btn-success btn-lg" value="submit" />			
+									
+				</form>
+			</div>
+		</div>
+	
+	</div>
 
-<p> hahaha, actually, I don't like this page.</p>
-
-<strong>at all.</strong>
 
 
 
