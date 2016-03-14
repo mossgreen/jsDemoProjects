0291828155cd868f8a700681ed9aaca0f3a1b1ee
Moss Green <gufeifei@outlook.com>
2016-03-14 22:40:24 +1300

    send email using php

--- Udemy/php/index.php
+++ Udemy/php/index.php
@@ -1,3 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+  
+	<body>
+		<div>
+			<?php
+				$emailTo = "";
+				$subject = "I hope this works!";
+				$body = "I think you're great";
+				$headers = "From: rob@robpercival.co.uk";
+				
+				if(mail($emailTo, $subject, $body, $headers) == 1){
+					echo "Mail sent";
+				}else{
+					echo "Mail failed";
+				}
+			
+  	
+  	
+  			?>
+		</div>
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
 
 echo" --how to define arrays in php--";
@@ -92,21 +141,5 @@ while($array[$i]){
 	$i ++;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
+
