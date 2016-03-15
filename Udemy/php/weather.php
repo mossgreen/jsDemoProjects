2ec29de4f6ea0787335ea5af6f2711336658cf06
Moss Green <gufeifei@outlook.com>
2016-03-15 19:31:56 +1300

    weather project, added fixed responsive background

---
+++ Udemy/php/weather.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+	<title>My weather page</title>
+	
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	
+	<style>
+	
+		html,body{
+			height:100%;
+		}
+		.container{
+			background-image:url("background.jpg");
+			width:100%;
+			height:100%;
+			background-size:cover;
+			background-position:center;
+		}
+	
+	</style>
+
+
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-med -6 col-md-offset-3">
+				
+			
+			</div>
+			
+		</div>
+	
+	</div>
+
+
+
+
+
+
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
