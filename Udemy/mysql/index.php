87c0f4cc7bee65e901564d6c5615da9e768b8fe6
Moss Green <gufeifei@outlook.com>
2016-03-16 17:06:53 +1300

    print error message with mysql in php page

---
+++ Udemy/mysql/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//will print error message on the page
+//since the localhoste is a typo
+mysqli_connect("localhoste", "cl54-users-4w9", "JFrM7d-d-");
+
+echo mysqli_connect_error();
+
+
+?>
