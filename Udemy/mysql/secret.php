61ee07d6c0485a761fe4f5b22a1292969ce10620
Moss Green <gufeifei@outlook.com>
2016-03-18 20:13:24 +1300

    bug fixed.

--- Udemy/mysql/secret.php
+++ Udemy/mysql/secret.php
@@ -8,7 +8,7 @@
 		setcookie("id", "", time()-60*60);
 		$_COOKIE["id"] = "";
 		
-	}else if((array_key_exists("id", $_SESSION) AND $_SESSION['id']) OR ( (array_key_exists("id", $_COOKIE) AND $_COOKIE['id'])){
+	}else if((array_key_exists("id", $_SESSION) AND $_SESSION['id']) OR (array_key_exists("id", $_COOKIE) AND $_COOKIE['id'])){
 		header("Location: loggedinpage.php");
 	}
 
