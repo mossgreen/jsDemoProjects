72f1290e468af81def1da1277de4e308583d1faf
Moss Green <gufeifei@outlook.com>
2016-03-19 22:55:33 +1300

    add "connection.php", "footer.php"

---
+++ Udemy/mysql/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+	
+		$link=mysqli_connect("localhost", "cl54-secretdi", "NqUq^/bhe", "cl54-secretdi");
+	
+		if(mysqli_connect_error()){
+			die("database connect error");
+		}
+		
+		
+		
+?>
