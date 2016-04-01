57affd89b995c120eb5175cc5e85e50262c22cac
Moss Green <gufeifei@outlook.com>
2016-04-01 16:29:51 +1300

    delete info derived from other website, will use api

--- Udemy/APIs/index.php
+++ Udemy/APIs/index.php
@@ -5,42 +5,7 @@
     
     if ($_GET['city']) {
         
-        $city = str_replace(' ', '', $_GET['city']);
         
-        $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-        
-        
-        if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
-    
-            $error = "That city could not be found.";
-
-        } else {
-        
-        $forecastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-        
-        $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
-            
-        if (sizeof($pageArray) > 1) {
-        
-                $secondPageArray = explode('</span></span></span>', $pageArray[1]);
-            
-                if (sizeof($secondPageArray) > 1) {
-
-                    $weather = $secondPageArray[0];
-                    
-                } else {
-                    
-                    $error = "That city could not be found.";
-                    
-                }
-            
-            } else {
-            
-                $error = "That city could not be found.";
-            
-            }
-        
-        }
         
     }
 
