079d7dd6637a1a7361a5803df99dd53cb4058068
Moss Green <gufeifei@outlook.com>
2016-06-26 15:46:14 +1200

    done with sliding door effect

--- iMooc/DOM/Lesson1/scripts/slidingdoors.js
+++ iMooc/DOM/Lesson1/scripts/slidingdoors.js
@@ -17,7 +17,28 @@ var boxWidth = imgWidth + (imgs.length -1) * exposeWidth;
 box.style.width = boxWidth + 'px';
 
 //set each doors positon
-for(var i = 1, len = imgs.length; i< len; i++){
-	imgs[i].style.left=  imgWidth + exposeWidth *(i-1) + 'px';
+function setImgsPos(){
+	for(var i = 1, len = imgs.length; i< len; i++){
+		imgs[i].style.left=  imgWidth + exposeWidth *(i-1) + 'px';
+	}
+}
+
+setImgsPos();
+
+//calculate each doors distance while sliding
+var translate = imgWidth - exposeWidth;
+
+//bind each pic with event
+for(var i = 0, len = imgs.length; i< len; i++){
+	(function(i){
+		imgs[i].onmouseover = function(){
+			setImgsPos();
+			for(var j = 1; j<= i; j++){
+				imgs[j].style.left = parseInt(imgs[j].style.left,10) - translate +'px';
+			}
+		}
+
+
+	})(i);
 }
 };
