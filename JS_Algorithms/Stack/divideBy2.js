5d437f5e8375f16e2bd8bc82f9973570c5f7599a
Moss Green <gufeifei@outlook.com>
2016-07-01 18:37:51 +1200

    test divideBy2 method

---
+++ JS_Algorithms/Stack/divideBy2.js
@@ -0,0 +1,21 @@
+
+function divideBy2(decNumber){
+	var remStack = new Stack(),
+	rem,
+	binaryString='';
+
+	while(decNumber > 0){
+		rem = Math.floor(decNumber % 2);
+		console.log("rem = " + rem);
+		remStack.push(rem);
+		
+		decNumber = Math.floor(decNumber/2);
+		console.log("decNumber is: "+decNumber.toString());
+	}
+
+	while(!remStack.isEmpty()){
+		binaryString += remStack.pop().toString();
+	}
+
+	return binaryString;
+}
