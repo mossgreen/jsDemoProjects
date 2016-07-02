ef32a6b5c4e7acdf17d85699063dfd423ec934ae
Moss Green <gufeifei@outlook.com>
2016-07-02 16:39:36 +1200

    add priorityQueue.js

---
+++ JS_Algorithms/Queue/priorityQueue.js
@@ -0,0 +1,53 @@
+function PriorityQueue(){
+	var items = [];
+
+	function QueueElement (element, priority){
+		this.element = element;
+		this.priority = priority;
+	}
+
+	this.enqueue = function(element, priority){
+		var queueElement = new QueueElement(element, priority);
+	}
+
+	if(this.isEmpty()){
+		items.push(queueElement);
+	}else{
+		var added = false;
+		for(var i=0; i< items.length; i++){
+			if(queueElement.priority < items[i].priority){
+				items.splice(i,0,queueElement);
+				added = true;
+				break;
+			}
+		}
+
+		if(!added){
+			items.push(queueElement);
+		}
+	}
+	
+	this.dequeue = function(){
+		items.shift();
+	};
+
+	this.front = function(){
+		return items[0];
+	};
+
+	this.isEmpty = function(){
+		return items.length == 0;
+	};
+
+	this.clear = function(){
+		items = [];
+	};
+
+	this.size = function(){
+		return items.length;
+	};
+
+	this.print = function(){
+		console.log(items.toString());
+	};
+}
