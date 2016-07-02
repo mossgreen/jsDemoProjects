d6efae3dec24098f5d6e89f042d027c5af599ebc
Moss Green <gufeifei@outlook.com>
2016-07-02 16:26:29 +1200

    add queue.js

---
+++ JS_Algorithms/Queue/queue.js
@@ -0,0 +1,31 @@
+function Queue(){
+	var items = [];
+
+	this.enqueue = function(element){
+		items.push(element);
+	};
+
+	this.dequeue = function(){
+		items.pop();
+	};
+
+	this.front = function(){
+		return items[0];
+	};
+
+	this.isEmpty = function(){
+		return items.length == 0;
+	};
+
+	this.clear = function(){
+		items = [];
+	};
+
+	this.size = function(){
+		return items.length;
+	};
+
+	this.print = function(){
+		console.log(items.toString());
+	};
+}
