99aebf5afa09c74ac9a5a816934f5d926c8553ee
Moss Green <gufeifei@outlook.com>
2016-07-08 19:03:44 +1200

    js_algorithms/ queue_errors
    inner is: undefined

---
+++ JS_Algorithms/Queue/hotPotato.js
@@ -0,0 +1,22 @@
+alert("hi");
+
+
+function hotPotato(nameList, num){
+    var queue = new Queue();
+
+    for(var i = 0; i< nameList.length; i++){
+        queue.enqueue(nameList[i]);
+    }
+
+    var eliminated = '';
+    while(queue.size() > 1){
+        for(var i = 0; i< num; i ++){
+            queue.enqueue(queue.dequeue());
+        }
+
+        eliminated = queue.dequeue();
+        console.log(eliminated + "deleted from this game.");
+    }
+
+    return queue.dequeue();
+}
