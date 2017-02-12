9c66d78bfdb8892fa7c868e0ea19108ee22f40d7
Feifei GU <gufeifei@outlook.com>
2017-02-13 10:39:13 +1300

    Add auth0 example

---
+++ 01-login/app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'YOUR_CLIENT_ID',
+    domain: 'YOUR_DOMAIN'
+};
